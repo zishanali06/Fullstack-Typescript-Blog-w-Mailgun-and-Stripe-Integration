4ae538a2778afe249dfc1023bfced99557ae18aa
Zishan Ali <zishanali06@gmail.com>
2019-04-17 11:56:40 -0500

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Full Stack Blog using MySQL and Authetification
+Adding Stripe to My Blog, adding on top of version with authentification.
+
---
+++ src/server/routes/api/index.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+import * as passport from 'passport';
+
+//api route imports
+import apiBlogRoutes from './blogs';
+import apiDonateRoutes from './donate';
+
+const router = express.Router();
+
+router.use('/donate', apiDonateRoutes);
+
+router.use((req, res, next) => {
+    passport.authenticate('bearer', { session: false}, (err, user, info) => {
+        if(err) console.log(err);
+        if(user) req.user = user;
+        return next();
+    })(req, res, next);
+});
+
+router.use('/blogs', apiBlogRoutes);
+
+export default router;
